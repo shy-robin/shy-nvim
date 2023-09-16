101cead1305a5131d785cb84db444e1248b8f255
ShyRobin <shy_robin@163.com>
2023-09-16 22:08:02 +0800

    feat: move lines keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,6 +3,7 @@
 -- Add any additional keymaps here
 
 local set = vim.keymap.set
+local del = vim.keymap.del
 
 -- windows
 set("n", "<leader>wb", "<C-W>s", { desc = "Split window below", remap = true })
@@ -13,3 +14,15 @@ set("n", "<leader>wk", "5<C-W>+", { desc = "Increase window height", remap = tru
 set("n", "<leader>wj", "5<C-W>-", { desc = "Decrease window height", remap = true })
 set("n", "<leader>wh", "5<C-W><", { desc = "Decrease window width", remap = true })
 set("n", "<leader>wl", "5<C-W>>", { desc = "Increase window width", remap = true })
+
+-- remove default Move lines
+del("n", "<A-j>")
+del("n", "<A-k>")
+del("i", "<A-j>")
+del("i", "<A-k>")
+del("v", "<A-j>")
+del("v", "<A-k>")
+
+-- move lines
+set("v", "J", ":move '>+1<cr>gv=gv", { desc = "Move down", silent = true })
+set("v", "K", ":move '<-2<cr>gv=gv", { desc = "Move up", silent = true })
