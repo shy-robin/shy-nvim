927e5e8e68a74fd59076398267505219b7db183f
ShyRobin <shy_robin@163.com>
2023-09-18 00:04:20 +0800

    feat: nvim-lspconfig config

---
+++ lua/plugins/nvim-lspconfig.lua
@@ -0,0 +1,21 @@
+return {
+  "neovim/nvim-lspconfig",
+  init = function()
+    local format = function()
+      require("lazyvim.plugins.lsp.format").format({ force = true })
+    end
+
+    local keys = require("lazyvim.plugins.lsp.keymaps").get()
+    -- change a keymap
+    keys[#keys + 1] = { "gh", vim.lsp.buf.hover, desc = "Hover" }
+    keys[#keys + 1] = { "gj", "]d", desc = "Next Diagnostic", remap = true }
+    keys[#keys + 1] = { "gk", "[d", desc = "Prev Diagnostic", remap = true }
+    keys[#keys + 1] = { "gl", vim.diagnostic.open_float, desc = "Line Diagnostics" }
+    keys[#keys + 1] = { "gne", "]e", desc = "Next Error", remap = true }
+    keys[#keys + 1] = { "gNe", "[e", desc = "Prev Error", remap = true }
+    keys[#keys + 1] = { "gnw", "]w", desc = "Next Warning", remap = true }
+    keys[#keys + 1] = { "gNw", "[w", desc = "Prev Warning", remap = true }
+    keys[#keys + 1] = { "<leader>fm", format, desc = "Format Document", has = "formatting" }
+    keys[#keys + 1] = { "<leader>fm", format, desc = "Format Range", mode = "v", has = "rangeFormatting" }
+  end,
+}
