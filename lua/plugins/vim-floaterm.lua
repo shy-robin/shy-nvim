369ba7c147f8f4f614f6b5d5033a5887c21d0290
ShyRobin <shy_robin@163.com>
2023-09-23 11:50:32 +0800

    feat: vim-floaterm config

---
+++ lua/plugins/vim-floaterm.lua
@@ -0,0 +1,124 @@
+return {
+  "voldikss/vim-floaterm",
+  keys = {
+    {
+      "<C-o>",
+      "<cmd>FloatermToggle<cr>",
+      desc = "Toggle Floaterm",
+    },
+    {
+      "<C-o>",
+      "<cmd>FloatermToggle<cr>",
+      desc = "Toggle Floaterm",
+      mode = "t",
+    },
+    {
+      "<C-k>",
+      "<cmd>FloatermNew<cr>",
+      desc = "New Floaterm",
+      mode = "t",
+    },
+    {
+      "<C-h>",
+      "<cmd>FloatermPrev<cr>",
+      desc = "Prev Floaterm",
+      mode = "t",
+    },
+    {
+      "<C-l>",
+      "<cmd>FloatermNext<cr>",
+      desc = "Next Floaterm",
+      mode = "t",
+    },
+    {
+      "<C-f>",
+      "<cmd>FloatermFirst<cr>",
+      desc = "First Floaterm",
+      mode = "t",
+    },
+    {
+      "<C-e>",
+      "<cmd>FloatermLast<cr>",
+      desc = "Last Floaterm",
+      mode = "t",
+    },
+    {
+      "<C-n>",
+      "<C-\\><C-n>",
+      desc = "Enter Normal Mode",
+      mode = "t",
+    },
+    {
+      "<C-j>",
+      function()
+        local w = vim.g.floaterm_width
+        if w == 0.95 then
+          vim.api.nvim_command("FloatermUpdate --height=0.6 --width=0.6")
+          vim.g.floaterm_width = 0.6
+          vim.g.floaterm_height = 0.6
+        else
+          vim.api.nvim_command("FloatermUpdate --height=0.95 --width=0.95")
+          vim.g.floaterm_width = 0.95
+          vim.g.floaterm_height = 0.95
+        end
+      end,
+      desc = "Toggle Floaterm Size",
+      mode = "t",
+    },
+    {
+      "<C-q>",
+      "<cmd>FloatermKill<cr>",
+      desc = "Quit Floaterm",
+      mode = "t",
+    },
+    {
+      "<leader>tb",
+      "<cmd>FloatermNew --wintype=split --height=20<cr>",
+      desc = "Toggle Bottom Terminal",
+    },
+    {
+      "<leader>tr",
+      "<cmd>FloatermNew --wintype=vsplit --width=80<cr>",
+      desc = "Toggle Right Terminal",
+    },
+    {
+      "<leader>tob",
+      "<cmd>FloatermNew --cwd=<buffer><cr>",
+      desc = "Open Floaterm in Current Buffer",
+    },
+    {
+      "<leader>tor",
+      "<cmd>FloatermNew ranger<cr>",
+      desc = "Open Ranger Floaterm",
+    },
+    {
+      "<leader>top",
+      "<cmd>FloatermNew python3<cr>",
+      desc = "Open Python3 Floaterm",
+    },
+    {
+      "<leader>ton",
+      "<cmd>FloatermNew node<cr>",
+      desc = "Open Nodejs Floaterm",
+    },
+    {
+      "<leader>tol",
+      "<cmd>FloatermNew lua<cr>",
+      desc = "Open Lua Floaterm",
+    },
+    {
+      "<leader>gg",
+      "<cmd>FloatermNew lazygit<cr>",
+      desc = "Lazygit (root dir)",
+    },
+  },
+  config = function()
+    vim.g.floaterm_borderchars = "─│─│╭╮╯╰"
+    -- 设置从 floaterm 打开文件的状态
+    vim.g.floaterm_opener = "edit"
+
+    local hl = vim.api.nvim_set_hl
+    -- 设置 floaterm 失去焦点后的前景色
+    hl(0, "FloatermNC", { fg = "gray" })
+  end,
+}
