7d687fa04a470fb3e8a821354616fcfa9b16d36d
ShyRobin <shy_robin@163.com>
2023-09-25 00:24:07 +0800

    feat: colorscheme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "sainnhe/everforest",
+  },
+  {
+    "folke/tokyonight.nvim",
+    -- lazy = true,
+    -- opts = { style = "night" },
+  },
+  {
+    "navarasu/onedark.nvim",
+    -- lazy = true,
+    -- opts = { style = "cool" },
+  },
+  {
+    "catppuccin/nvim",
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
