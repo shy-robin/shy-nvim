a3b4a145126a64d86c042291e71b3dfd19d229b9
ShyRobin <shy_robin@163.com>
2023-09-24 23:24:17 +0800

    feat: flash config

---
+++ lua/plugins/flash.lua
@@ -0,0 +1,41 @@
+return {
+  "folke/flash.nvim",
+  opts = {
+    modes = {
+      char = {
+        keys = {
+          ";",
+          ",",
+        },
+      },
+    },
+  },
+  keys = {
+    {
+      "s",
+      mode = { "n", "x", "o" },
+      false,
+    },
+    {
+      "S",
+      mode = { "n", "o", "x" },
+      false,
+    },
+    {
+      "f",
+      mode = { "n", "x", "o" },
+      function()
+        require("flash").jump()
+      end,
+      desc = "Flash",
+    },
+    {
+      "F",
+      mode = { "n", "o", "x" },
+      function()
+        require("flash").treesitter()
+      end,
+      desc = "Flash Treesitter",
+    },
+  },
+}
