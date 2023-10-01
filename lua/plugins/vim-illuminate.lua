f62ade1a730bba0be5c924102fddf4b16513e354
ShyRobin <shy_robin@163.com>
2023-10-01 22:28:01 +0800

    fix: illuminate load event

--- lua/plugins/vim-illuminate.lua
+++ lua/plugins/vim-illuminate.lua
@@ -1,6 +1,6 @@
 return {
   "RRethy/vim-illuminate",
-  event = "BufRead",
+  event = "BufReadPre",
   init = function()
     -- 高亮相同的单词，highlight 链接到 Visual
     vim.api.nvim_set_hl(0, "IlluminatedWordText", { link = "Visual" })
