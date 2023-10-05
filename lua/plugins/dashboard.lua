60db82470114674293bda8eb72c724851bef838f
ShyRobin <shy_robin@163.com>
2023-10-06 00:08:13 +0800

    feat: dashboard shortcuts

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -7,25 +7,29 @@ return {
         enable = true
       },
       shortcut = {
-        { desc = " Plugins", group = "@property", action = "Lazy", key = "p" },
         {
-          desc = " Files",
-          group = "Label",
+          desc = "Files",
+          icon = " ",
           action = "Telescope find_files",
           key = "f",
         },
         {
-          desc = " Text",
-          group = "DiagnosticHint",
+          desc = "Text",
+          icon = " ",
           action = "Telescope live_grep",
           key = "t",
         },
+        { action = "Lazy", desc = " Lazy", icon = "󰒲 ", key = "l" },
+        { action = "ene | startinsert", desc = "New", icon = " ", key = "n" },
         {
-          desc = " Scheme",
-          group = "Number",
+          desc = "Scheme",
+          icon = " ",
           action = "Telescope colorscheme",
           key = "s",
         },
+        { action = "e $MYVIMRC", desc = "Config", icon = " ", key = "c" },
+        { action = 'lua require("persistence").load()', desc = "Restore", icon = "󰦛 ", key = "r" },
+        { action = "qa", desc = "Quit", icon = " ", key = "q" },
       },
     },
     hide = {
