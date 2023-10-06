429158d8971a9b68c9e540de212684c67258e11b
ShyRobin <shy_robin@163.com>
2023-10-06 16:13:10 +0800

    feat: scroll off

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,7 +2,13 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options
 
+local opt = vim.opt
+
 -- 指定拼写检查的语言
-vim.opt.spelllang = { 'en', 'cjk' }
+opt.spelllang = { 'en', 'cjk' }
 -- 开启拼写检查
-vim.opt.spell = true
+opt.spell = true
+
+-- 滚动边距，影响 zt zb
+opt.scrolloff = 0 -- Lines of context
+opt.sidescrolloff = 0 -- Columns of context
