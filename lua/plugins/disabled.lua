f38a97a365935ccfee9dbf73f10ef5bd745a2614
ShyRobin <shy_robin@163.com>
2023-10-06 23:26:43 +0800

    refactor: disable plugins

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,30 +1,20 @@
-return {
-  {
-    "nvimtools/none-ls.nvim",
-    enabled = false,
-  },
-  {
-    "goolord/alpha-nvim",
-    enabled = false,
-  },
-  {
-    "RRethy/vim-illuminate",
-    enabled = false,
-  },
-  {
-    "williamboman/mason.nvim",
-    enabled = false,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    enabled = false,
-  },
-  {
-    "hrsh7th/nvim-cmp",
-    enabled = false,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    enabled = false,
-  }
+local plugins = {
+  "nvimtools/none-ls.nvim",
+  "goolord/alpha-nvim",
+  "RRethy/vim-illuminate",
+  "williamboman/mason.nvim",
+  "williamboman/mason-lspconfig.nvim",
+  "hrsh7th/nvim-cmp",
+  "neovim/nvim-lspconfig",
 }
+
+local disabled_plugins = {}
+
+for _, value in pairs(plugins) do
+  table.insert(disabled_plugins, {
+    value,
+    enabled = false,
+  })
+end
+
+return disabled_plugins
