a9dfb116e57858bfa29ea379a50ddfab32ed7dfa
ShyRobin <shy_robin@163.com>
2023-10-07 20:08:18 +0800

    feat: disable indentscope in floaterm

---
+++ lua/plugins/indentscope.lua
@@ -0,0 +1,23 @@
+return {
+  "echasnovski/mini.indentscope",
+  init = function()
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = {
+        "help",
+        "alpha",
+        "dashboard",
+        "neo-tree",
+        "Trouble",
+        "lazy",
+        "mason",
+        "notify",
+        "toggleterm",
+        "lazyterm",
+        "floaterm"
+      },
+      callback = function()
+        vim.b.miniindentscope_disable = true
+      end,
+    })
+  end,
+}
