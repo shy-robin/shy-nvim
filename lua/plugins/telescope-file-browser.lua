05007849a44b9527cb49be8e3dedc8b47fb13875
ShyRobin <shy_robin@163.com>
2023-10-10 21:15:34 +0800

    feat: telescope file browser

---
+++ lua/plugins/telescope-file-browser.lua
@@ -0,0 +1,21 @@
+return {
+  "nvim-telescope/telescope-file-browser.nvim",
+  dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+  event = "VeryLazy",
+  keys = {
+    {
+      "<leader>fb",
+      "<cmd>Telescope file_browser<cr>",
+      noremap = true,
+      silent = true,
+      desc = "File Browser (cwd)",
+    },
+    {
+      "<leader>fB",
+      "<cmd>Telescope file_browser path=%:p:h select_buffer=true<cr>",
+      noremap = true,
+      silent = true,
+      desc = "File Browser (Current Buffer)",
+    },
+  },
+}
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,3 +1,32 @@
+-- see: https://github.com/nvim-telescope/telescope.nvim/issues/2201
+local ts_select_dir_for_grep = function(prompt_bufnr)
+  local action_state = require("telescope.actions.state")
+  local fb = require("telescope").extensions.file_browser
+  local live_grep = require("telescope.builtin").live_grep
+  local current_line = action_state.get_current_line()
+
+  fb.file_browser({
+    files = false,
+    depth = false,
+    attach_mappings = function(prompt_bufnr)
+      require("telescope.actions").select_default:replace(function()
+        local entry_path = action_state.get_selected_entry().Path
+        local dir = entry_path:is_dir() and entry_path or entry_path:parent()
+        local relative = dir:make_relative(vim.fn.getcwd())
+        local absolute = dir:absolute()
+
+        live_grep({
+          results_title = relative .. "/",
+          cwd = absolute,
+          default_text = current_line,
+        })
+      end)
+
+      return true
+    end,
+  })
+end
+
 return {
   "nvim-telescope/telescope.nvim",
   event = "VeryLazy",
@@ -60,5 +89,22 @@ return {
         },
       },
     },
+    pickers = {
+      live_grep = {
+        mappings = {
+          i = {
+            ["<C-f>"] = ts_select_dir_for_grep,
+          },
+          n = {
+            ["<C-f>"] = ts_select_dir_for_grep,
+          },
+        },
+      },
+    },
   },
+  config = function(_, opts)
+    local telescope = require("telescope")
+    telescope.setup(opts)
+    telescope.load_extension("file_browser")
+  end,
 }
