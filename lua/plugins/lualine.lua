f129e4c4e9fb31f041826d60e2e0e539aeefb967
ShyRobin <shy_robin@163.com>
2023-10-12 20:23:14 +0800

    fix: lualine sometimes show while startup

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,6 +1,6 @@
 return {
   "nvim-lualine/lualine.nvim",
-  event = "VeryLazy",
+  event = "UIEnter",
   opts = function()
     local icons = require("lazyvim.config").icons
     local Util = require("lazyvim.util")
