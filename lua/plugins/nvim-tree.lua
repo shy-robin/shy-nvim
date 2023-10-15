3ccc98a720b9c3bb67da9e13a9cbcdb1d7cd525b
ShyRobin <shy_robin@163.com>
2023-10-15 15:16:48 +0800

    feat: not close tree after open file

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,17 +1,18 @@
-local function edit_or_open()
-  local api = require("nvim-tree.api")
-  local node = api.tree.get_node_under_cursor()
-
-  if node.nodes ~= nil then
-    -- expand or collapse folder
-    api.node.open.edit()
-  else
-    -- open file
-    api.node.open.edit()
-    -- Close the tree if file was opened
-    api.tree.close()
-  end
-end
+-- close tree automatically when open file
+-- local function edit_or_open()
+--   local api = require("nvim-tree.api")
+--   local node = api.tree.get_node_under_cursor()
+--
+--   if node.nodes ~= nil then
+--     -- expand or collapse folder
+--     api.node.open.edit()
+--   else
+--     -- open file
+--     api.node.open.edit()
+--     -- Close the tree if file was opened
+--     api.tree.close()
+--   end
+-- end
 
 -- open as vsplit on current node
 local function vsplit_preview()
@@ -42,7 +43,7 @@ local function my_on_attach(bufnr)
 
   -- custom mappings
   set("n", "?", api.tree.toggle_help, opts("Help"))
-  set("n", "l", edit_or_open, opts("Edit Or Open"))
+  set("n", "l", api.node.open.edit, opts("Edit Or Open"))
   set("n", "L", vsplit_preview, opts("Vsplit Preview"))
   set("n", "h", api.node.navigate.parent_close, opts("Close"))
 
