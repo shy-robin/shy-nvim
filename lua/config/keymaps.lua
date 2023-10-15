4720790984225de840ca3f8dce2c6b01d18802ee
ShyRobin <shy_robin@163.com>
2023-10-15 18:05:46 +0800

    chore: remove keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -55,9 +55,9 @@ set("v", "p", '"_dp', { desc = "Paste with no register" })
 set("c", "<C-j>", "<C-n>", { desc = "Select Next Item", remap = true })
 set("c", "<C-k>", "<C-p>", { desc = "Select Prev Item", remap = true })
 
--- spell check
-set("n", "gns", "]s", { desc = "Next misspelled word", remap = true, silent = true })
-set("n", "gNs", "[s", { desc = "Prev misspelled word", remap = true, silent = true })
+-- spell check (use coc-spell-checker instead)
+-- set("n", "gns", "]s", { desc = "Next misspelled word", remap = true, silent = true })
+-- set("n", "gNs", "[s", { desc = "Prev misspelled word", remap = true, silent = true })
 
 -- reload
 set("n", "<leader>rh", "<cmd>syntax sync fromstart<cr>", { desc = "Reload Syntax Highlight", silent = true })
