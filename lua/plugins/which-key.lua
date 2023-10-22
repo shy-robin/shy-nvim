0d4aa417000f4dd2cb8f11b5daca1de618ba146f
ShyRobin <shy_robin@163.com>
2023-10-22 21:42:49 +0800

    feat: which key operator

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -4,5 +4,15 @@ return {
     window = {
       border = "rounded",
     },
-  }
+  },
+  config = function(_, opts)
+    local wk = require("which-key")
+
+    local presets = require("which-key.plugins.presets")
+    -- 设置 operator
+    presets.operators["t"] = "Tabs"
+
+    wk.setup(opts)
+    wk.register(opts.defaults)
+  end,
 }
