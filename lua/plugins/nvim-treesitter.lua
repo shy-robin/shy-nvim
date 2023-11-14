97936fcf8a92bbe70287876815a1b5b7616cbafe
ShyRobin <shy_robin@163.com>
2023-11-14 21:44:33 +0800

    feat: disable treesitter when open big files

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,6 +1,16 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = false,
+      -- 当打开大文件时，禁用 treesitter，防止卡顿
+      disable = function(_, bufnr)
+        local buf_name = vim.api.nvim_buf_get_name(bufnr)
+        local file_size = vim.api.nvim_call_function("getfsize", { buf_name })
+        return file_size > 256 * 1024
+      end,
+    },
     ensure_installed = {
       "bash",
       "c",
