700584c7071e367b0254306d6306390720383153
ShyRobin <shy_robin@163.com>
2024-05-02 20:39:31 +0800

    feat: change error icons

--- lua/plugins/lazy-vim.lua
+++ lua/plugins/lazy-vim.lua
@@ -4,7 +4,7 @@ return {
     opts = {
       icons = {
         diagnostics = {
-          Error = " ",
+          Error = " ",
           Warn = " ",
           Hint = " ",
           Info = " ",
