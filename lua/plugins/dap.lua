58b8a62c3c39e29460b5e5c399d1e77d2d4107a8
ShyRobin <shy_robin@163.com>
2024-06-24 22:38:49 +0800

    feat: pin version of vscode-js-debug

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -73,6 +73,7 @@ return {
           "microsoft/vscode-js-debug",
           -- After install, build it and rename the dist directory to out
           build = "npm install --legacy-peer-deps --no-save && npx gulp vsDebugServerBundle && rm -rf out && mv dist out",
+          pin = true,
           -- version = "1.*",
         },
         -- 支持 json5 （注释等新特性）
