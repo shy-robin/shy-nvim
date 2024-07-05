1c2e139f9722b6e2a2074f2f076026e1371e08d9
ShyRobin <shy_robin@163.com>
2024-07-06 01:14:41 +0800

    feat: outline icons

--- lua/plugins/outline.lua
+++ lua/plugins/outline.lua
@@ -3,9 +3,27 @@ return {
   lazy = true,
   cmd = { "Outline", "OutlineOpen" },
   keys = {
-    { "<leader>o", "<cmd>Outline<CR>", desc = "Toggle outline" },
+    { "<leader>o", "<cmd>Outline<CR>", desc = "Toggle Outline" },
   },
-  opts = {
-    -- Your setup opts here
-  },
+  opts = function()
+    local defaults = require("outline.config").defaults
+    local opts = {
+      symbols = {
+        icons = {},
+        filter = LazyVim.config.kind_filter,
+      },
+      keymaps = {
+        up_and_jump = "<up>",
+        down_and_jump = "<down>",
+      },
+    }
+
+    for kind, symbol in pairs(defaults.symbols.icons) do
+      opts.symbols.icons[kind] = {
+        icon = LazyVim.config.icons.kinds[kind] or symbol.icon,
+        hl = symbol.hl,
+      }
+    end
+    return opts
+  end,
 }
