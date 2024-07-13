032ad7ca1663fc085c1927ed27dbe366894b5cc8
ShyRobin <shy_robin@163.com>
2024-07-13 21:48:09 +0800

    feat: which key config

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,25 +1,70 @@
 return {
   "folke/which-key.nvim",
   opts = {
-    window = {
+    win = {
       border = "rounded",
     },
-    defaults = {
-      ["<leader>D"] = { name = "+diffview" },
-      ["<leader>C"] = { name = "+coc" },
-      ["<leader>ca"] = { name = "+code-action" },
-      ["<leader>cb"] = { name = "+comment-box" },
-      ["<leader>cs"] = { name = "+snippets" },
-      ["<leader>Ct"] = { name = "+translator" },
+    spec = {
+      {
+        "<leader>D",
+        group = "+diffview",
+      },
+      {
+        "<leader>C",
+        group = "+coc",
+      },
+      {
+        "<leader>ca",
+        group = "+code-action",
+      },
+      {
+        "<leader>cb",
+        group = "+comment-box",
+      },
+      {
+        "<leader>cs",
+        group = "+snippets",
+      },
+      {
+        "<leader>Ct",
+        group = "+translator",
+      },
+      {
+        "<leader>t",
+        group = "+tabs/terminal",
+      },
+      {
+        "<leader>to",
+        group = "+open",
+      },
+      {
+        "<leader>m",
+        group = "+marks",
+      },
+      {
+        "<leader>md",
+        group = "+delete",
+      },
+      {
+        "<leader>r",
+        group = "+refactor",
+      },
+    },
+    modes = {
+      -- terminal 模式下禁用，否则按 esc 无法退出一些功能
+      t = false,
+    },
+    icons = {
+      rules = false,
     },
   },
   config = function(_, opts)
     local wk = require("which-key")
 
-    local presets = require("which-key.plugins.presets")
-    -- 设置 operator
-    presets.operators["m"] = "Marks"
-    presets.operators["t"] = "Tabs"
+    -- local presets = require("which-key.plugins.presets")
+    -- -- 设置 operator
+    -- presets.operators["m"] = "Marks"
+    -- presets.operators["t"] = "Tabs"
 
     wk.setup(opts)
     wk.register(opts.defaults)
