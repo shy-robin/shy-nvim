f3213171c98b7d4596de285cf70157e06244ccc4
ShyRobin <shy_robin@163.com>
2024-07-13 22:01:43 +0800

    feat: which key config

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -58,15 +58,4 @@ return {
       rules = false,
     },
   },
-  config = function(_, opts)
-    local wk = require("which-key")
-
-    -- local presets = require("which-key.plugins.presets")
-    -- -- 设置 operator
-    -- presets.operators["m"] = "Marks"
-    -- presets.operators["t"] = "Tabs"
-
-    wk.setup(opts)
-    wk.register(opts.defaults)
-  end,
 }
