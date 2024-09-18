b527eaf97e77f964c061b8250a7985ba3550647c
ShyRobin <shy_robin@163.com>
2024-09-18 19:30:58 +0800

    feat: toggle env shell

---
+++ astro-to-bak.sh
@@ -0,0 +1,9 @@
+mv ~/.config/nvim ~/.config/nvim.astro
+mv ~/.local/share/nvim ~/.local/share/nvim.astro
+mv ~/.local/state/nvim ~/.local/state/nvim.astro
+mv ~/.cache/nvim ~/.cache/nvim.astro
+
+mv ~/.config/nvim.bak ~/.config/nvim
+mv ~/.local/share/nvim.bak ~/.local/share/nvim
+mv ~/.local/state/nvim.bak ~/.local/state/nvim
+mv ~/.cache/nvim.bak ~/.cache/nvim
---
+++ bak-to-astro.sh
@@ -0,0 +1,9 @@
+mv ~/.config/nvim ~/.config/nvim.bak
+mv ~/.local/share/nvim ~/.local/share/nvim.bak
+mv ~/.local/state/nvim ~/.local/state/nvim.bak
+mv ~/.cache/nvim ~/.cache/nvim.bak
+
+mv ~/.config/nvim.astro ~/.config/nvim
+mv ~/.local/share/nvim.astro ~/.local/share/nvim
+mv ~/.local/state/nvim.astro ~/.local/state/nvim
+mv ~/.cache/nvim.astro ~/.cache/nvim
