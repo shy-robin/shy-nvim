52ca87a9f784d71826383ea69165ddcf02624b7f
ShyRobin <shy_robin@163.com>
2024-12-02 01:36:28 +0800

    feat: add global debug function

--- init.lua
+++ init.lua
@@ -1,2 +1,11 @@
+-- 参考：https://github.com/folke/snacks.nvim/blob/main/docs/debug.md
+_G.dd = function(...)
+  Snacks.debug.inspect(...)
+end
+_G.bt = function()
+  Snacks.debug.backtrace()
+end
+vim.print = _G.dd
+
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
