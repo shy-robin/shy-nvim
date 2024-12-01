fc61ace141f87694aa716326379ddf696c938de0
ShyRobin <shy_robin@163.com>
2024-12-02 02:31:03 +0800

    feat: custom dashboard

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -2,6 +2,7 @@ return {
   "folke/snacks.nvim",
   opts = {
     dashboard = {
+      width = 93,
       formats = {
         key = function(item)
           return { { "[", hl = "special" }, { item.key, hl = "key" }, { "]", hl = "special" } }
@@ -10,10 +11,47 @@ return {
       autokeys = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890", -- autokey sequence
       sections = {
         { section = "header" },
-        { section = "startup", padding = 1 },
-        { section = "keys", gap = 1 },
+        {
+          text = { { os.date("%Y-%m-%d %H:%M:%S"), hl = "@property" } },
+          align = "center",
+          padding = 1,
+        },
+        -- 自定义文本，使其放在一行
+        {
+          align = "center",
+          padding = 1,
+          text = {
+            { " File [f] ", hl = "@property" },
+            { " New [n] ", hl = "@property" },
+            { " Text [g] ", hl = "@property" },
+            { " Recent [r] ", hl = "@property" },
+            { " Config [c] ", hl = "@property" },
+            { " Session [s] ", hl = "@property" },
+            { "󰒲 Lazy [l] ", hl = "@property" },
+            { " Quit [q]", hl = "@property" },
+          },
+        },
+        -- 隐藏快捷键
+        { key = "f", action = ":lua Snacks.dashboard.pick('files')", hidden = true },
+        { key = "n", action = ":ene | startinsert", hidden = true },
+        { key = "g", action = ":lua Snacks.dashboard.pick('live_grep')", hidden = true },
+        { key = "r", action = ":lua Snacks.dashboard.pick('oldfiles')", hidden = true },
+        {
+          key = "c",
+          action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+          hidden = true,
+        },
+        { key = "s", section = "session", hidden = true },
+        { key = "l", action = ":Lazy", enabled = package.loaded.lazy ~= nil, hidden = true },
+        { key = "q", action = ":qa", hidden = true },
+
         { icon = " ", title = "Recent Files", section = "recent_files", indent = 3, padding = { 2, 2 } },
         { icon = " ", title = "Projects", section = "projects", indent = 3, padding = 2 },
+        { section = "startup", padding = 1 },
+        {
+          align = "center",
+          text = "🚀 Sharp tools make good work.",
+        },
       },
     },
     bigfile = {
