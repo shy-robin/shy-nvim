d395c5f721f6b6f9dd6d85c0e981581e745eeabf
ShyRobin <shy_robin@163.com>
2024-12-04 00:12:55 +0800

    feat: move global functions

--- init.lua
+++ init.lua
@@ -1,11 +1,2 @@
--- 参考：https://github.com/folke/snacks.nvim/blob/main/docs/debug.md
-_G.dd = function(...)
-  Snacks.debug.inspect(...)
-end
-_G.bt = function()
-  Snacks.debug.backtrace()
-end
-vim.print = _G.dd
-
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -65,4 +65,20 @@ return {
       backdrop = 100,
     },
   },
+  init = function()
+    -- 参考：https://github.com/folke/snacks.nvim/blob/main/docs/debug.md
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "VeryLazy",
+      callback = function()
+        -- Setup some globals for debugging (lazy-loaded)
+        _G.dd = function(...)
+          Snacks.debug.inspect(...)
+        end
+        _G.bt = function()
+          Snacks.debug.backtrace()
+        end
+        vim.print = _G.dd -- Override print to use snacks for `:=` command
+      end,
+    })
+  end,
 }
