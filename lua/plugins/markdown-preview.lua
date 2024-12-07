55ae68c0e8921ff59387d2235f374762b0c05e7a
ShyRobin <shy_robin@163.com>
2024-12-07 22:30:46 +0800

    feat: preview markdown config

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -8,6 +8,8 @@ return {
     vim.g.mkdp_browser = ""
     vim.g.mkdp_echo_preview_url = true
     vim.g.mkdp_page_title = "「${name}」"
+    vim.g.mkdp_auto_close = 0
+    vim.g.mkdp_theme = "light"
   end,
   ft = { "markdown" },
   keys = { { "gom", "<cmd>MarkdownPreviewToggle<cr>", desc = "Markdown Preview" } },
