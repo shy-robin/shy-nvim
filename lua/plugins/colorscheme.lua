99f659abb6aa39295ced60cca7379d51eeb69b6c
ShyRobin <shy_robin@163.com>
2024-12-12 15:24:42 +0800

    feat: add color scheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -67,6 +67,10 @@ return {
     event = "VeryLazy",
   },
   {
+    "lunarvim/darkplus.nvim",
+    event = "VeryLazy",
+  },
+  {
     "LazyVim/LazyVim",
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
