e7552c78926033502faa964ee1d3f572e72ea512
ShyRobin <shy_robin@163.com>
2024-12-14 21:55:16 +0800

    feat: remove unuseful colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -6,15 +6,7 @@ return {
   {
     "folke/tokyonight.nvim",
     event = "VeryLazy",
-    -- lazy = true,
-    -- opts = { style = "night" },
   },
-  -- {
-  --   "navarasu/onedark.nvim",
-  --   event = "VeryLazy",
-  --   lazy = true,
-  --   opts = { style = "cool" },
-  -- },
   {
     "catppuccin/nvim",
     event = "VeryLazy",
@@ -28,19 +20,11 @@ return {
     event = "VeryLazy",
   },
   {
-    "Th3Whit3Wolf/one-nvim",
-    event = "VeryLazy",
-  },
-  {
     "rockerBOO/boo-colorscheme-nvim",
     event = "VeryLazy",
   },
   {
     "marko-cerovac/material.nvim",
-    event = "VeryLazy",
-  },
-  {
-    "RRethy/nvim-base16",
     event = "VeryLazy",
   },
   {
