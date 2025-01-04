646ba4c3ee2f48dbd6dd92ace4a06fce36b9ed92
ShyRobin <shy_robin@163.com>
2025-01-05 01:11:32 +0800

    feat: nvim colorizer

--- lua/plugins/nvim-colorizer.lua
+++ lua/plugins/nvim-colorizer.lua
@@ -1,7 +1,12 @@
 return {
-  "NvChad/nvim-colorizer.lua",
+  "catgoose/nvim-colorizer.lua",
   event = "VeryLazy",
-  config = function()
-    require("colorizer").setup()
-  end,
+  opts = {
+    filetypes = {
+      "*",
+    },
+    user_default_options = {
+      css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+    },
+  },
 }
