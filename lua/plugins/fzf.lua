b58f692f9d028746237821d50c6deea63bacfbdd
ShyRobin <shy_robin@163.com>
2025-01-05 02:31:31 +0800

    feat: change fzf default keymaps

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -4,5 +4,17 @@ return {
     winopts = {
       backdrop = 100,
     },
+    keymap = {
+      builtin = {
+        true,
+        ["<Esc>"] = "hide", -- hide fzf-lua, `:FzfLua resume` to continue
+        ["<C-h>"] = "toggle-help",
+        ["<C-m>"] = "toggle-fullscreen",
+        ["<C-p>"] = "toggle-preview",
+      },
+    },
+    fzf_opts = {
+      ["--cycle"] = true,
+    },
   },
 }
