a5f0c4ed0555c130cc68551d1f83f810beb39530
ShyRobin <shy_robin@163.com>
2025-01-25 18:02:34 +0800

    feat: add orgmode

---
+++ lua/plugins/orgmode.lua
@@ -0,0 +1,9 @@
+return {
+  "nvim-orgmode/orgmode",
+  event = "VeryLazy",
+  ft = { "org" },
+  opts = {
+    org_agenda_files = "~/orgfiles/**/*",
+    org_default_notes_file = "~/orgfiles/refile.org",
+  },
+}
