2b1d115d41c79751ea8604955733e84201e6d683
ShyRobin <shy_robin@163.com>
2025-01-25 23:07:44 +0800

    feat: org bullets

--- lua/plugins/orgmode.lua
+++ lua/plugins/orgmode.lua
@@ -1,9 +1,15 @@
 return {
   "nvim-orgmode/orgmode",
+  dependencies = {
+    "nvim-orgmode/org-bullets.nvim",
+  },
   event = "VeryLazy",
   ft = { "org" },
-  opts = {
-    org_agenda_files = "~/orgfiles/**/*",
-    org_default_notes_file = "~/orgfiles/refile.org",
-  },
+  config = function()
+    require("orgmode").setup({
+      org_agenda_files = "~/orgfiles/**/*",
+      org_default_notes_file = "~/orgfiles/refile.org",
+    })
+    require("org-bullets").setup()
+  end,
 }
