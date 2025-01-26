10e690c7e16bfb91dc3c81ee7fcc519989ea017b
ShyRobin <shy_robin@163.com>
2025-01-27 00:56:59 +0800

    feat: add org roam

--- lua/plugins/orgmode.lua
+++ lua/plugins/orgmode.lua
@@ -2,6 +2,7 @@ return {
   "nvim-orgmode/orgmode",
   dependencies = {
     "nvim-orgmode/org-bullets.nvim",
+    "chipsenkbeil/org-roam.nvim",
   },
   event = "VeryLazy",
   ft = { "org" },
@@ -9,7 +10,14 @@ return {
     require("orgmode").setup({
       org_agenda_files = "~/orgfiles/**/*",
       org_default_notes_file = "~/orgfiles/refile.org",
+      org_startup_folded = "showeverything",
     })
     require("org-bullets").setup()
+    require("org-roam").setup({
+      directory = "~/org-roam-files",
+      org_files = {
+        "~/orgfiles/**/*.org",
+      },
+    })
   end,
 }
