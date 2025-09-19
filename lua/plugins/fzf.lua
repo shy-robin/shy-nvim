93059129fb1efb3da51d23ede9d78a0e09fda814
ShyRobin <shy_robin@163.com>
2025-09-19 19:05:32 +0800

    feat: fzf 快捷键

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -1,3 +1,5 @@
+local actions = require("fzf-lua").actions
+
 return {
   "ibhagwan/fzf-lua",
   opts = {
@@ -11,11 +13,27 @@ return {
         ["<C-h>"] = "toggle-help",
         -- 不能使用 <C-m> 因为它是 enter 的等效快捷键
         ["<C-l>"] = "toggle-fullscreen",
-        ["<C-p>"] = "toggle-preview",
+        ["<C-o>"] = "toggle-preview",
+        ["<C-n>"] = "preview-page-down",
+        ["<C-p>"] = "preview-page-up",
       },
     },
     fzf_opts = {
       ["--cycle"] = true,
+    },
+    actions = {
+      -- Below are the default actions, setting any value in these tables will override
+      -- the defaults, to inherit from the defaults change [1] from `false` to `true`
+      files = {
+        true, -- uncomment to inherit all the below in your custom config
+        -- 将所选文件添加到 quickfix 列表，使用 :copen 打开列表（使用 ctrl+i 多选文件）
+        ["ctrl-t"] = actions.file_sel_to_qf,
+        -- 将所选文件添加到 location 列表，使用 :lopen 打开列表（使用 ctrl+i 多选文件）
+        ["ctrl-y"] = actions.file_sel_to_ll,
+        ["ctrl-w"] = actions.toggle_hidden,
+        ["ctrl-e"] = { fn = actions.toggle_ignore },
+        ["ctrl-r"] = actions.toggle_follow,
+      },
     },
   },
 }
