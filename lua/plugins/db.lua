a698a0cdc93f855f32463d9872258b52f30df8d7
ShyRobin <shy_robin@163.com>
2026-01-03 21:48:25 +0800

    docs: dabdod

--- lua/plugins/db.lua
+++ lua/plugins/db.lua
@@ -12,5 +12,14 @@ return {
   init = function()
     -- Your DBUI configuration
     vim.g.db_ui_use_nerd_fonts = 1
+
+    -- NOTE: 有一些云数据库的 mysql 版本可能比较老，需要通过过 `brew install mariadb` 安装支持旧版本的 mysql
+    -- 同时需要加上 ssl=0 参数，关闭 mysql 的 ssl 加密
+    -- vim.g.dbs = {
+    --   {
+    --     name = "test",
+    --     url = "mysql://username:password@host:port/database?ssl=0",
+    --   },
+    -- }
   end,
 }
