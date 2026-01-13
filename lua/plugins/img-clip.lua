e6ef4e366147bddf4ef02ef1ea7aeceb3e8f9ec7
ShyRobin <shy_robin@163.com>
2026-01-13 20:29:03 +0800

    fix: 禁用终端缓冲区中的 img-clip 拖放功能

--- lua/plugins/img-clip.lua
+++ lua/plugins/img-clip.lua
@@ -2,8 +2,14 @@ return {
   "HakonHarnes/img-clip.nvim",
   event = "VeryLazy",
   opts = {
-    -- add options here
-    -- or leave it empty to use the default settings
+    default = {
+      drag_and_drop = {
+        -- 在终端缓冲区中禁用，避免粘贴文字时报错
+        enabled = function()
+          return vim.bo.buftype ~= "terminal"
+        end,
+      },
+    },
   },
   keys = {
     -- suggested keymap
