02b67f3ab4acfdab5240e1a071a0b88a3445d051
shy-robin <shy_robin@163.com>
2026-03-09 19:50:54 +0800

    feat: mason 弹窗样式

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,50 +1,61 @@
 return {
-  "LazyVim/LazyVim",
-  opts = {
-    mason = {
-      ensure_installed = {
-        -- LSP servers
-        "typescript-tools",
-        "vue-language-server",
-        "lua-language-server",
-        "json-lsp",
-        "yaml-language-server",
-        "html-lsp",
-        "css-lsp",
-        "svelte-language-server",
-        "pyright",
-        "ruff",
-        "gopls",
-        "rust-analyzer",
-        "tailwindcss-language-server",
-        "emmet-language-server",
-        
-        -- Formatters
-        "prettierd",
-        "stylua",
-        "black",
-        "isort",
-        "gofumpt",
-        "rustfmt",
-        "sqlfmt",
-        "shfmt",
-        
-        -- Linters
-        "eslint_d",
-        "markdownlint-cli2",
-        "shellcheck",
-        "golangci-lint",
-        "hadolint",
-        
-        -- Debuggers
-        "debugpy",
-        "delve",
-        
-        -- DAP adapters
-        "codelldb",
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      mason = {
+        ensure_installed = {
+          -- LSP servers
+          "typescript-tools",
+          "vue-language-server",
+          "lua-language-server",
+          "json-lsp",
+          "yaml-language-server",
+          "html-lsp",
+          "css-lsp",
+          "svelte-language-server",
+          "pyright",
+          "ruff",
+          "gopls",
+          "rust-analyzer",
+          "tailwindcss-language-server",
+          "emmet-language-server",
+
+          -- Formatters
+          "prettierd",
+          "stylua",
+          "black",
+          "isort",
+          "gofumpt",
+          "rustfmt",
+          "sqlfmt",
+          "shfmt",
+
+          -- Linters
+          "eslint_d",
+          "markdownlint-cli2",
+          "shellcheck",
+          "golangci-lint",
+          "hadolint",
+
+          -- Debuggers
+          "debugpy",
+          "delve",
+
+          -- DAP adapters
+          "codelldb",
+        },
+        ui = {
+          border = "rounded",
+        },
       },
+    },
+  },
+  {
+    "mason-org/mason.nvim",
+    opts = {
       ui = {
         border = "rounded",
+        backdrop = 100,
       },
     },
   },
