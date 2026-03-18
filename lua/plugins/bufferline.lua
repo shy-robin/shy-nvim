48bbc97317b3919402153259c73e0ca509c7aa59
shy-robin <shy_robin@163.com>
2026-03-18 18:45:21 +0800

    feat: 移除 bufferline 配置（使用 lazyvim 默认配置）

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -2,8 +2,6 @@ return {
   "akinsho/bufferline.nvim",
   opts = {
     options = {
-      always_show_bufferline = false,
-      diagnostics = "nvim_lsp",
       offsets = {
         {
           filetype = "NvimTree",
@@ -12,36 +10,8 @@ return {
           text_align = "center",
         },
       },
-      buffer_close_icon = "󰅖",
       show_buffer_close_icons = false,
       show_close_icon = false,
-    },
-  },
-  keys = {
-    { "<leader>bh", "<cmd>bprevious<cr>", desc = "Prev Buffer" },
-    { "<leader>bl", "<cmd>bnext<cr>", desc = "Next Buffer" },
-    { "tt", "<cmd>BufferLinePick<cr>", desc = "Pick buffer" },
-    { "tn", "<cmd>enew<cr>", desc = "New buffer" },
-    { "tH", "<cmd>BufferLineMovePrev<cr>", desc = "Move buffer left" },
-    { "tL", "<cmd>BufferLineMoveNext<cr>", desc = "Move buffer right" },
-    { "tp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle pin" },
-    { "tP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete non-pinned buffers" },
-    {
-      "tw",
-      function()
-        require("mini.bufremove").delete(0, false)
-      end,
-      desc = "Delete Buffer",
-    },
-    {
-      "tW",
-      "<cmd>bd!<cr>",
-      desc = "Delete Buffer (Force)",
-    },
-    {
-      "to",
-      "<cmd>BufferLineCloseOthers<cr>",
-      "Delete other buffers",
     },
   },
 }
